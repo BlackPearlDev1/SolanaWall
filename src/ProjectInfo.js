9983c377603ce0aba494d2e08100943cae31f457
bmi <158223871+melierc20@users.noreply.github.com>
2024-11-09 14:51:07 +0100

    dev

--- src/ProjectInfo.js
+++ src/ProjectInfo.js
@@ -21,6 +21,7 @@ const ProjectInfo = () => {
                 Project:
                 <a href="https://whitepaper.solwall.live/sol-wall-project/user-guide" target="_blank" rel="noopener noreferrer" style={{ color: '#9945FF' }}>User guide</a>
                 <a href="https://whitepaper.solwall.live" target="_blank" rel="noopener noreferrer" style={{ color: '#9945FF' }}>White paper</a>
+                <a href="https://rugcheck.xyz/tokens/B47jrQkyMsG7wEnri3iud4i5MQAaWQkgqTevrCU9Lj6R" target="_blank" rel="noopener noreferrer" style={{ color: '#9945FF' }}>RugCheck</a>
                 <a href="https://github.com/BlackPearlDev1/SolanaWall" target="_blank" rel="noopener noreferrer" style={{ color: '#9945FF' }}>Github</a>
                 <a href="mailto:team@solwall.live" style={{ color: '#9945FF' }}>Contact</a>
             </div>
@@ -37,8 +38,8 @@ const ProjectInfo = () => {
             </div>
             <div style={{ display: 'flex', justifyContent: 'center', gap: '10px' }}>
                 Charts:
-                {/* <a href="https://www.dextools.io/app/en/solana/pair-explorer/B47jrQkyMsG7wEnri3iud4i5MQAaWQkgqTevrCU9Lj6R" target="_blank" rel="noopener noreferrer" style={{ color: '#9945FF' }}>Dextools</a> */}
                 <a href="https://dexscreener.com/solana/B47jrQkyMsG7wEnri3iud4i5MQAaWQkgqTevrCU9Lj6R" target="_blank" rel="noopener noreferrer" style={{ color: '#9945FF' }}>Dexscreener</a>
+                <a href="https://www.dextools.io/app/en/solana/pair-explorer/B47jrQkyMsG7wEnri3iud4i5MQAaWQkgqTevrCU9Lj6R" target="_blank" rel="noopener noreferrer" style={{ color: '#9945FF' }}>DEXTools</a>
                 <a href="https://birdeye.so/token/B47jrQkyMsG7wEnri3iud4i5MQAaWQkgqTevrCU9Lj6R" target="_blank" rel="noopener noreferrer" style={{ color: '#9945FF' }}>Birdeye</a>
             </div>
         </div>
